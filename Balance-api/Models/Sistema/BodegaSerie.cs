329313d6b6ef0f7d92a3733992f9853d265c6d69
JairMartinez86 <jmartinezg86@gmail.com>
2023-09-04 14:27:35 -0600

    Servidor

---
+++ Balance-api/Models/Sistema/BodegaSerie.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Balance_api.Models.Sistema
+{
+    [Table("BodegaSerie", Schema = "SIS")]
+    public class BodegaSerie
+    {
+        [Key]
+        public int IdBodegaSerie { get; set; }
+        public string CodBodega { get; set; }
+        public string Serie { get; set; }
+        public string CodUnion { get; set; }
+        public string SerieUnion { get; set; }
+        public bool EsFact { get; set; }
+        public bool EsColector { get; set; }
+        public bool EsPedido { get; set; }
+        public bool EsCaja { get; set; }
+        public bool EsRequisa { get; set; }
+        public bool EsInv { get; set; }
+        public bool EsExport { get; set; }
+        public bool EsImport { get; set; }
+        public bool EsPlaneacion { get; set; }
+        public bool EsConsignacion { get; set; }
+    }
+}
---
+++ Balance-api/Models/Sistema/TipoDocumento.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Balance_api.Models.Sistema
+{
+    [Table("TipoDocumento", Schema = "CNT")]
+    public class TipoDocumento
+    {
+        [Key]
+        public int IdTipoDocumento { get; set; }
+        public string Abreviatura { get; set; }
+        public string Descripcion { get; set; }
+        public bool Manual { get; set; }
+    }
+}
