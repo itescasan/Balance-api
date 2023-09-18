f3910e4e392294c18529f288f89e01493f9770f1
JairMartinez86 <jmartinezg86@gmail.com>
2023-09-18 12:32:17 -0600

    Asiento

--- Balance-api/Controllers/Contabilidad/AuxiliarContableController.cs
+++ Balance-api/Controllers/Contabilidad/AuxiliarContableController.cs
@@ -73,5 +73,78 @@ namespace Balance_api.Controllers.Contabilidad
 
             return json;
         }
+
+
+
+        [Route("api/Contabilidad/AuxiliarContable/GetAsiento")]
+        [HttpGet]
+        public string GetAsiento(string NoDoc, string Serie)
+        {
+            return V_GetAsiento(NoDoc, Serie);
+        }
+
+        private string V_GetAsiento(string NoDoc, string Serie)
+        {
+       
+
+            string json = string.Empty;
+            try
+            {
+                using (Conexion)
+                {
+                    List<Cls_Datos> lstDatos = new();
+
+                    var qAsiento = (from _q in Conexion.AsientosContables
+                                    where (_q.NoDocOrigen == null ? _q.NoAsiento : _q.NoDocOrigen) == NoDoc && (_q.IdSerieDocOrigen == null ? _q.IdSerie : _q.IdSerieDocOrigen) == Serie
+                                    select new
+                                    {
+                                        _q.IdAsiento,
+                                        _q.IdPeriodo,
+                                        _q.NoAsiento,
+                                        _q.IdSerie,
+                                        _q.Fecha,
+                                        _q.IdMoneda,
+                                        _q.TasaCambio,
+                                        _q.Concepto,
+                                        _q.NoDocOrigen,
+                                        _q.IdSerieDocOrigen,
+                                        _q.TipoDocOrigen,
+                                        _q.Bodega,
+                                        _q.Referencia,
+                                        _q.Estado,
+                                        _q.TipoAsiento,
+                                        _q.Total,
+                                        _q.TotalML,
+                                        _q.TotalMS,
+                                        _q.FechaReg,
+                                        _q.UsuarioReg,
+                                        _q.AsientosContablesDetalle
+
+                                    }).Take(1);
+
+
+                    Cls_Datos datos = new();
+                    datos.Nombre = "ASIENTO";
+                    datos.d = qAsiento;
+
+                    lstDatos.Add(datos);
+
+
+                    json = Cls_Mensaje.Tojson(lstDatos, lstDatos.Count, string.Empty, string.Empty, 0);
+                }
+
+
+
+            }
+            catch (Exception ex)
+            {
+                json = Cls_Mensaje.Tojson(null, 0, "1", ex.Message, 1);
+            }
+
+            return json;
+        }
+
+
+
     }
 }
