3bf5591203b8b5d119d32ed336cb11ac1e9a3a0a
JairMartinez86 <jmartinezg86@gmail.com>
2023-09-19 13:13:35 -0600

    auxiliar

---
+++ Balance-api/Controllers/Inventario/BodegaController.cs
@@ -0,0 +1,69 @@
+﻿using Balance_api.Class;
+using Balance_api.Contexts;
+using Balance_api.Controllers.Sistema;
+using Balance_api.Models.Contabilidad;
+using Balance_api.Models.Sistema;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Transactions;
+
+namespace Balance_api.Controllers.Inventario
+{
+    public class BodegaController : Controller
+    {
+        private readonly BalanceEntities Conexion;
+
+        public BodegaController(BalanceEntities db)
+        {
+            Conexion = db;
+        }
+
+        [Route("api/Inventario/Bodega/Get")]
+        [HttpGet]
+        public string Get()
+        {
+            return V_Get();
+        }
+
+        private string V_Get()
+        {
+            string json = string.Empty;
+            try
+            {
+                using (Conexion)
+                {
+                    List<Cls_Datos> lstDatos = new();
+
+
+                    var qBogas = (from _q in Conexion.Bodegas
+                                  select new
+                                  {
+                                      _q.IdBodega,
+                                      _q.Codigo,
+                                      _q.Bodega
+                                  }).ToList();
+
+                    Cls_Datos datos = new();
+                    datos.Nombre = "BODEGAS";
+                    datos.d = qBogas;
+
+                    lstDatos.Add(datos);
+
+
+                    json = Cls_Mensaje.Tojson(lstDatos, lstDatos.Count, string.Empty, string.Empty, 0);
+                }
+
+
+
+            }
+            catch (Exception ex)
+            {
+                json = Cls_Mensaje.Tojson(null, 0, "1", ex.Message, 1);
+            }
+
+            return json;
+        }
+
+
+    }
+}
