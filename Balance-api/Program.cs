3aab04391197397810ecf2ed567168326c8aa569
JairMartinez86 <jmartinezg86@gmail.com>
2023-11-21 13:53:30 -0600

    cambio

--- Balance-api/Program.cs
+++ Balance-api/Program.cs
@@ -18,9 +18,11 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
                       policy =>
                       {
-                          policy.WithOrigins("http://localhost:4200",
-                                              "http://www.jmg.com")
-                          .AllowAnyHeader()             
+                         // policy.AllowAnyOrigin()
+                         policy.WithOrigins("http://localhost:4200",
+                                             "http://localhost:150",
+                                             "http://165.98.96.131:150")
+                         .AllowAnyHeader()             
                           .AllowAnyMethod(); 
                       });
 });
@@ -38,20 +40,46 @@ builder.Services.AddDbContext<BalanceEntities>(options => options.UseSqlServer(b
 var app = builder.Build();
 
 
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+
 }
+
+
 
 app.UseHttpsRedirection();
 
 app.UseCors(MyAllowSpecificOrigins);
 
-
+app.UseAuthentication();
 app.UseAuthorization();
+
+app.UseStaticFiles();
+
+app.UseSwagger();
+
+app.UseSwaggerUI( c => {
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Balance API");
+});
+
+
+// Redirect requests to the root URL to the Swagger UI
+app.Use(async (context, next) => { 
+
+    if(context.Request.Path == "/")
+    {
+        context.Response.Redirect("/swagger/index.html");
+        return;
+    }
+
+    await next();
+} );
+
+
+
+
 
 app.MapControllers();
 
